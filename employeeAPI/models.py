ed5dcaad0305e8fdd15410f170b856187da4ea5e
Simo Siren <simo.siren@stud.pointcollege.fi>
2020-07-26 09:04:41 +0300

    employee model and db migrations done

--- employeeAPI/models.py
+++ employeeAPI/models.py
@@ -1,3 +1,10 @@
 from django.db import models
 
 # Create your models here.
+
+class Employee(models.Model):
+    employee_code = models.CharField(max_length=3)
+    firstname = models.CharField(max_length=50)
+    lastname = models.CharField(max_length=50)
+    phone = models.CharField(max_length=15)
+    position = models.CharField(max_length=20)
